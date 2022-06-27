d934c9a3554ccfd1a19ad8d94c732641eda0be35
Jean Baek <jeanbaek710@gmail.com>
2022-06-27 17:55:24 +0900

    added programmers lv.1 algorithm. lv.1은 확실히 쉽게, 남들보다 좋은 복잡도로 풀린다

---
+++ etc/lotto.js
@@ -0,0 +1,15 @@
+function solution(lottos, win_nums) {
+  const ranking = [6, 6, 5, 4, 3, 2, 1];
+  const winNumSet = new Set(win_nums);
+  let matchCount = 0;
+  let unknownNumCount = 0;
+
+  lottos.forEach((num) => {
+    if (num === 0) unknownNumCount++;
+    else if (winNumSet.has(num)) matchCount++;
+  });
+
+  return [ranking[matchCount + unknownNumCount], ranking[matchCount]];
+}
+
+console.log(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]));
