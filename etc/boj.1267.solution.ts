6fae3ff217ff2d2f1524b30cd4e2035926fb2100
Jean Baek <67567007+JeanBaek@users.noreply.github.com>
2022-08-21 00:08:14 +0900

    Update: boj.1267.solution.ts

--- etc/boj.1267.solution.ts
+++ etc/boj.1267.solution.ts
@@ -2,14 +2,14 @@ const fs = require('fs');
 const filepath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = fs.readFileSync(filepath).toString().trim().split('\n');
 
-function solution([len, ...times]: string[]) {
+function solution([len, times]: string[]) {
     let yCount = 0;
     let mCount = 0;
     
-    times.forEach((t) => {
-        yCount += Math.ceil( +t / 30 ); 
-        mCount += Math.ceil( +t / 60 ); 
-    })
+    times.split(' ').forEach((t) => {
+        yCount += Math.ceil( +t / 30 ) + +!(+t % 30); 
+        mCount += Math.ceil( +t / 60 ) + +!(+t % 60);    
+    });
     
     const yPhoneBill = 10 * yCount;
     const mPhoneBill = 15 * mCount;
