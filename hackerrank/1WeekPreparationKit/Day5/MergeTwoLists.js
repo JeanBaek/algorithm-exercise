76a2c68127ab5c38165f03b0d705958504c73dd9
Jean Baek <jeanbaek710@gmail.com>
2022-08-30 16:18:02 +0900

    Edit: Retried to solve 'MergeTwoLists' problem. It was not easy haha,,, I should focus on understanding the requirement entirely before solving the problem.

--- hackerrank/1WeekPreparationKit/Day5/MergeTwoLists.js
+++ hackerrank/1WeekPreparationKit/Day5/MergeTwoLists.js
@@ -1,84 +1,103 @@
+const SinglyLinkedListNode = class {
+	constructor(nodeData) {
+		this.data = nodeData;
+		this.next = null;
+	}
+};
+
+const SinglyLinkedList = class {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	insertNode(nodeData) {
+		const node = new SinglyLinkedListNode(nodeData);
+
+		if (this.head === null) {
+			this.head = node;
+		} else {
+			this.tail.next = node;
+		}
+
+		this.tail = node;
+	}
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+	console.log({ node });
+	while (node != null) {
+		console.log(node.data);
+
+		node = node.next;
+
+		if (node != null) {
+			console.log("");
+		}
+	}
+}
+
 // TODO: Retry and debug after 8/29 // Too difficult to debug in iPad....
 // ListNode: Class {data: number, next: ListNode}
 
 function mergeLists(head1, head2) {
-    // console.log(head1, head2)
-    // 0. make a new ListNode
-    // 1. compare two heads
-    // 2. add smaller node to a new ListNode
-    // 3. if one head value is null, add rest of another one to a new ListNode
-    
-    const mergeList = new SinglyLinkedList();
-    
-    let node1 = head1;
-    let node2 = head2;
-    
-    while (node1 && node2) {
-        console.log({node1, node2})
-        if (node1.data < node2.data) {
-            mergeList.insertNode(node1.data);
-            node1 = node1.next;
-        } else if (node1.data >= node2.data) {
-            mergeList.insertNode(node2.data);
-            node2 = node2.next;
-        } 
-        
-        if (node1 === null) {
-            mergeList.mergeList(node2);
-            node2 = null;
-        } else if (node2 === null) {
-            mergeList.mergeList(node1);
-            node1 = null;
-        }
-    }
-    
-    const returnlist = [];
-    
-    let node = mergeList.head;
-    
-    while(node) {
-        returnlist.push(node.data);
-        node = node.next;
-    }
-    
-    console.log({returnlist});
-    
-    return mergeList;
-}
+	// console.log(head1, head2)
+	// 0. make a new ListNode
+	// 1. compare two heads
+	// 2. add smaller node to a new ListNode
+	// 3. if one head value is null, add rest of another one to a new ListNode
 
-class SinglyLinkedListNode {
-    constructor(item) {
-        this.data = item;
-        this.next = null;
-    }
-}
+	const mergeList = new SinglyLinkedList();
+	mergeList.mergeList = function (node) {
+		if (this.head === null) {
+			this.head = node;
+		} else {
+			this.tail.next = node;
+		}
 
-class SinglyLinkedList {
-    constructor() {
-        this.head = null; // SinglyLinkedListNode
-    }
-    
-    insertNode(item) {
-        if (!this.head) {
-            this.head = new SinglyLinkedListNode(item);
-        } else {
-            this.head.next = new SinglyLinkedListNode(item);
-        }
-    }
-    
-    mergeList(list) {
-        if (!this.head) {
-            this.head = list;
-        } else {
-            this.head.next = list;
-        }
-    }
+		this.tail = node;
+	};
+
+	let node1 = head1;
+	let node2 = head2;
+
+	while (node1 || node2) {
+		if (node1 === null) {
+			mergeList.mergeList(node2);
+			node2 = null;
+		} else if (node2 === null) {
+			mergeList.mergeList(node1);
+			node1 = null;
+		}
+
+		if (node1?.data < node2?.data) {
+			mergeList.insertNode(node1.data);
+			node1 = node1.next;
+		} else if (node1?.data >= node2?.data) {
+			mergeList.insertNode(node2.data);
+			node2 = node2.next;
+		}
+	}
+
+	const returnlist = new SinglyLinkedList();
+
+	let node = mergeList.head;
+
+	while (node) {
+		returnlist.insertNode(node.data);
+		node = node.next;
+	}
+
+	return returnlist.head;
 }
 
 const list1 = new SinglyLinkedList();
 const list2 = new SinglyLinkedList();
 
-[1, 3, 7].forEach(item => list1.insertNode(item));
-[1, 2].forEach(item => list2.insertNode(item));
+// [1, 3, 7].forEach((item) => list1.insertNode(item));
+// [1, 2].forEach((item) => list2.insertNode(item));
 
-console.log(mergeLists(list1.head, list2.head));
+[1, 2, 3].forEach((item) => list1.insertNode(item));
+[3, 4].forEach((item) => list2.insertNode(item));
+
+printSinglyLinkedList(mergeLists(list1.head, list2.head));
