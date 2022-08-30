915b3d615ae20572c31ab5957e033b1c79a20741
Jean Baek <jeanbaek710@gmail.com>
2022-08-30 16:39:05 +0900

    Edit: Tried to optimize 'NewYearChaos' problem

--- hackerrank/1WeekPreparationKit/Day4/3.NewYearChaos.js
+++ hackerrank/1WeekPreparationKit/Day4/3.NewYearChaos.js
@@ -1,67 +1,46 @@
-'use strict';
-
-// TODO: time optimization
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/*
- * Complete the 'minimumBribes' function below.
- *
- * The function accepts INTEGER_ARRAY q as parameter.
- */
+const fs = require("fs");
+const input = fs
+	.readFileSync("./3.NewYearChaos.input.txt")
+	.toString()
+	.trim()
+	.split("\n");
 
 function minimumBribes(q) {
-    // Write your code here
-    // print 'order' earlier than its index + 1
-    // or print 'order' 2+ ealier than its index + 1
-    let bribeCount = 0;
-    let leftPosition = new Set(q);
-    
-    const isTooChaotic = q.some((order, index) => {
-        // if here's a too much bribed one, return true
-        /** 
+	// print 'order' earlier than its index + 1
+	// or print 'order' 2+ ealier than its index + 1
+	let bribeCount = 0;
+	let leftPosition = new Set(q);
+
+	const isTooChaotic = q.some((order, index) => {
+		// if here's a too much bribed one, return true
+		/** 
         e.g [2, 5, 1, 3, 4] 
         when index is 1 => there can be till 4
         if the 'order' is more than 4 
         **/
-        if (order - 2 > index + 1) return true;
-        if (order > index + 1)  bribeCount += (order - index - 1);
-        else if (order > Math.min(...leftPosition)) bribeCount += 1;
-        
-        leftPosition.delete(order);
-        
-        return false;
-    })
-    
-    console.log(isTooChaotic ? 'Too chaotic' : bribeCount);
+		if (order - 2 > index + 1) return true;
+		if (order > index + 1) bribeCount += order - index - 1;
+		else if (order > Math.min(...leftPosition)) bribeCount += 1;
+
+		leftPosition.delete(order);
+
+		return false;
+	});
+
+	console.log(isTooChaotic ? "Too chaotic" : bribeCount);
 }
 
-function main() {
-    const t = parseInt(readLine().trim(), 10);
+function main([caseCount, ...data]) {
+	const qList = [];
 
-    for (let tItr = 0; tItr < t; tItr++) {
-        const n = parseInt(readLine().trim(), 10);
+	for (let i = 0; i < +caseCount; i++) {
+		qList.push(data[i * 2 + 1].split(" "));
+	}
 
-        const q = readLine().replace(/\s+$/g, '').split(' ').map(qTemp => parseInt(qTemp, 10));
-
-        minimumBribes(q);
-    }
+	qList.forEach(minimumBribes);
 }
+
+main(input);
+
+// minimumBribes([2, 1, 5, 3, 4]);
+// minimumBribes([2, 5, 1, 3, 4]);
