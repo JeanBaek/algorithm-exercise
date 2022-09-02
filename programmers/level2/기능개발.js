a06a9640512e4a87d6ea8bda67b4043533085016
Jean Baek <jeanbaek710@gmail.com>
2022-09-03 00:00:46 +0900

    Edit: programmers 기능개발 풀이조건 수정

--- programmers/level2/기능개발.js
+++ programmers/level2/기능개발.js
@@ -2,7 +2,7 @@ function solution(progresses, speeds) {
 	return progresses
 		.map((p, i) => [Math.ceil((100 - p) / speeds[i]), 1])
 		.reduce((p, c, i) => {
-			if (i && p[p.length - 1][0] > c[0]) p[p.length - 1][1]++;
+			if (i && p[p.length - 1][0] >= c[0]) p[p.length - 1][1]++;
 			else p.push(c);
 
 			return p;
@@ -10,4 +10,6 @@ function solution(progresses, speeds) {
 		.map(([day, count]) => count);
 }
 
-console.log(solution([93, 30, 55], [1, 30, 5]));
+// console.log(solution([93, 30, 55], [1, 30, 5]));
+// console.log(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]));
+console.log(solution([1, 1, 50], [100, 2, 1]));
