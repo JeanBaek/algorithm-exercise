52a1984f82cbb98a64f7a2485ed1cba45875148b
jean <jean@floatic.io>
2023-03-02 09:09:04 +0900

    feat: 프로그래머스 level2 of tips - 짝지어 제거하기 재풀이 (2023.03.02)

--- programmers/level2/tips/짝지어_제거하기.js
+++ programmers/level2/tips/짝지어_제거하기.js
@@ -1,18 +1,19 @@
-const testCases = ['baabaa', 'cdcd', 'aabbccdd'];
+const testCases = ['baabaa', 'cdcd', 'aabbccdd', 'cdcdee', 'aaaa', 'akjhlaksjhaflksjhfksjdhfkja', 'abcacba'];
 
 const regExp2continuousW = /(\w)\1{1}/ig; // aa, bb, cc, dd, ee
 
 // TODO: 시간복잡도 줄여서 월요일에 소개하기
 
 function solution(s) {
-    let isAllPopped = false;
-
-    // 홀수면 모두 제거 불가
-    if (s.length % 2) return +isAllPopped;
-
-    // 짝수면 제거 시도
-    let poppedS = s;
-    while (poppedS) {
+    /** method #1
+     let isAllPopped = false;
+     //
+     // 홀수면 모두 제거 불가
+     if (s.length % 2) return +isAllPopped;
+     //
+     // 짝수면 제거 시도
+     let poppedS = s;
+     while (poppedS) {
         const replacedS = poppedS.replaceAll(regExp2continuousW, "");
 
         // 제거된 문자열 없고, 아직 문자 남아있으면 모두 제거 불가 판단
@@ -21,8 +22,22 @@ function solution(s) {
         poppedS = replacedS;
     }
 
-    isAllPopped = true;
-    return +isAllPopped;
+     isAllPopped = true;
+     return +isAllPopped;
+     **/
+
+        // method #2
+    const stack = [];
+
+    // 홀수면 모두 제거 불가
+    if (s.length % 2) return 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (stack[stack.length - 1] === s[i]) stack.pop();
+        else stack.push(s[i]);
+    }
+
+    return +!stack.length;
 }
 
-console.log(solution(testCases[0]));
+console.log(solution(testCases[6]));
